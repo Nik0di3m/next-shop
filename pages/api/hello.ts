eb8d2b9425ef719e8ffe0fc452e6d25d80e47d58
Nik0di3m <norbert.waleszczyk@icloud.com>
2022-07-04 20:47:48 +0200

    api switch method

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,14 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { PrismaClient } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
-
+import { prisma } from '../../lib/prisma'
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    const prismna = new PrismaClient()
+    const { method } = req
 
-    const users = await prismna.user.findMany()
-
-    res.status(200).json({ user: users })
+    switch (method) {
+        case 'GET':
+            try {
+                const users = await prisma.user.findMany()
+                res.status(200).json({ users: users })
+                console.error('ok')
+            } catch (e) {
+                console.error('Request error', e)
+            }
+            break
+        default:
+            res.setHeader('Allow', ['GET'])
+            res.status(405).end(`Method ${method} Not Allowed`)
+    }
 }
