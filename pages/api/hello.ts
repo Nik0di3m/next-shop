03fde1795cb549aedbe9944967a6bf09778115e2
Nik0di3m <norbert.waleszczyk@icloud.com>
2022-07-04 19:41:48 +0200

    first commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+    const prismna = new PrismaClient()
+
+    const users = await prismna.user.findMany()
+
+    res.status(200).json({ user: users })
 }
