ee5cd011f2f3f2f794e926366c681d958ea072d1
unknown <nikodem.waleszczyk@icloud.com>
2022-07-24 18:41:12 +0200

    deploy fix

--- components/NewsFeed/NewsFeed.tsx
+++ components/NewsFeed/NewsFeed.tsx
@@ -7,7 +7,9 @@ interface INewsFeed {
 const NewsFeed: FC<INewsFeed> = ({ children }) => {
     return (
         <div className="absolute max-w-lg space-y-4 italic top-1/3 left-20">
-            <h1 className="text-4xl font-semibold drop-shadow">What's new?</h1>
+            <h1 className="text-4xl font-semibold drop-shadow">
+                What{"'"}s new?
+            </h1>
             <div
                 tabIndex={0}
                 className="max-h-[528px] space-y-8 overflow-y-scroll pr-2 scrollbar-thin scrollbar-thumb-rounded-full"
