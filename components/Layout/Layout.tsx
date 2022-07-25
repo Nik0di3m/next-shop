7426440d68d5e7622ed9f1735abfb939e4dd7802
Nik0di3m <norbert.waleszczyk@icloud.com>
2022-07-25 22:19:27 +0200

    fix main container height

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -18,7 +18,7 @@ const Layout = ({ children }: Props) => {
     useEffect(() => setLogoColor(theme), [theme]);
 
     return (
-        <div className="flex h-[300vh]">
+        <div className="flex min-h-[300vh]">
             <div className="sticky top-0 left-0 flex flex-col items-center h-screen border-r 2xl:items-start 2xl:w-1/6 2xl:p-7 py-7 dark:border-black-600 dark:bg-black-700 bg-neutral-50">
                 <div className="hidden 2xl:flex">
                     <Logo
@@ -36,7 +36,7 @@ const Layout = ({ children }: Props) => {
                 </div>
             </div>
 
-            <div className="w-full">
+            <div className="w-full pb-8">
                 <Menu />
                 <div className="px-4">
                     <div className="py-8 mt-10 rounded-lg max-w-[1380px] dark:bg-black-700 bg-neutral-50 border dark:border-black-600 w-full mx-auto shadow-lg dark:shadow-pink-900/30">
